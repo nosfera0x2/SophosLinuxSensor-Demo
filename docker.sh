a730ff5e65dc98ae602ea2d3016cc1b839a40bc4
spence-rat <82817752+spence-rat@users.noreply.github.com>
2022-07-26 16:42:23 -0500

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl enable docker
